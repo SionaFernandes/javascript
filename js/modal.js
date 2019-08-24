1ca6ea75ecf2ce7af3a7778dcb35c143d552ded5
Siona Fernandes <sionac.fernandes@gmail.com>
2019-08-24 23:25:33 +0530

    popup modal using javascript

---
+++ js/modal.js
@@ -0,0 +1,31 @@
+// To Get the modal
+var modalbox = document.getElementById("custom-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalbtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modalbox.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalbox.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalbox) {
+    modalbox.style.display = "none";
+  }
+};
+
+var footerbtn = document.getElementById("btn-close");
+
+footerbtn.onclick = function() {
+  modalbox.style.display = "none";
+};
