d9ecbd7cc69fb340883b8c33777763fce9830d30
Siona Fernandes <sionac.fernandes@gmail.com>
2019-08-26 20:36:49 +0530

    animation added

--- js/modal.js
+++ js/modal.js
@@ -9,23 +9,23 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modalbox.style.display = "block";
+  modalbox.classList.add("custom-effect");
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modalbox.style.display = "none";
+  modalbox.classList.remove("custom-effect");
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modalbox) {
-    modalbox.style.display = "none";
+    modalbox.classList.remove("custom-effect");
   }
 };
 
 var footerbtn = document.getElementById("btn-close");
 
 footerbtn.onclick = function() {
-  modalbox.style.display = "none";
+  modalbox.classList.remove("custom-effect");
 };
