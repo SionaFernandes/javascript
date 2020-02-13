0c80a70df65451ab9af1bd9da973d9424cbc3538
Siona Fernandes <sionac.fernandes@gmail.com>
2020-02-13 20:14:14 +0530

    objects

--- array-object/js/app.js
+++ array-object/js/app.js
@@ -1,12 +1,18 @@
 let userInput = document.getElementById("user-input");
 let inputArray = [];
+let logEntries = [];
 
 let addButton = document.getElementById("add-btn");
 
 function pushtoarray() {
   inputArray.push(parseInt(userInput.value));
-  console.log(inputArray);
   document.getElementById("array-output").innerHTML = inputArray;
+  const logEntry = {
+    action: "Clicked",
+    value: parseInt(userInput.value)
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
 addButton.addEventListener("click", pushtoarray);
