651b17a4d1d26f88a5559ae4e7911c3eae9737a7
Siona Fernandes <sionac.fernandes@gmail.com>
2020-02-16 14:39:48 +0530

    removing array

--- array-object/js/app.js
+++ array-object/js/app.js
@@ -1,5 +1,6 @@
 let userInput = document.getElementById("user-input");
 let inputArray = [];
+let newArray = [];
 let logEntries = [];
 
 //push
@@ -13,6 +14,12 @@ let splicebtn = document.getElementById("splicebtn");
 //unshift
 let unshiftInput = document.getElementById("unshift-input");
 let unshiftbtn = document.getElementById("unshiftbtn");
+
+//pop
+let popButton = document.getElementById("popbtn");
+
+//shift
+let shiftButton = document.getElementById("shiftbtn");
 
 function pushtoarray() {
   inputArray.push(parseInt(userInput.value));
@@ -37,6 +44,21 @@ function unshiftarray() {
   document.getElementById("array-output").innerHTML = inputArray;
   unshiftInput.value = null;
 }
+
+function poparray() {
+  newArray.push(inputArray.pop());
+  document.getElementById("new-array-output").innerHTML = newArray;
+  document.getElementById("array-output").innerHTML = inputArray;
+}
+
+function shiftarray() {
+  newArray.push(inputArray.shift());
+  document.getElementById("new-array-output").innerHTML = newArray;
+  document.getElementById("array-output").innerHTML = inputArray;
+}
+
 addButton.addEventListener("click", pushtoarray);
 splicebtn.addEventListener("click", splicearray);
 unshiftbtn.addEventListener("click", unshiftarray);
+popButton.addEventListener("click", poparray);
+shiftButton.addEventListener("click", shiftarray);
