5067636e1aa8602fa615d7acbc2abc8120122ec7
Siona Fernandes <sionac.fernandes@gmail.com>
2020-02-22 23:29:39 +0530

    attacks

--- game-monster killer/assets/scripts/app.js
+++ game-monster killer/assets/scripts/app.js
@@ -1,4 +1,7 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 12;
+const STRONG_ATTACK_VALUE = 15;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,9 +9,51 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxdamage;
+  if (mode === "ATTACK") {
+    maxdamage = ATTACK_VALUE;
+  } else {
+    maxdamage = STRONG_ATTACK_VALUE;
+  }
+
+  const damage = dealMonsterDamage(maxdamage);
   currentMonsterHealth -= damage;
+  endRound();
+}
+
+function endRound() {
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You win!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("You have a draw!");
+  }
+}
+
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+function healPlayerHandler() {
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You cannot heal anymore");
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth += HEAL_VALUE;
+  endRound();
 }
 
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
